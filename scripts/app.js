9bfdef23e8ece059f81a89261d50cb5d33ccc5b7
Aniruddha wagle k <anirudha.ra21@sahyadri.edu.in>
2025-12-25 20:14:49 +0530

    done with step 2

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,11 @@
-console.log("E-Commerce Website Loaded");
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+if(hamburger) {
+    hamburger.addEventListener('click', () => {
+        // Toggle the active class on the nav list
+        navLinks.classList.toggle('nav-active');
+    });
+}
+
+console.log("App Loaded: Mobile Menu Ready");
